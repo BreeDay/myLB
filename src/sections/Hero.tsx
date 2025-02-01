ad589dc234b6e7fd752785e5f250d35e30dedc59
BreeDay <breeonad@gmail.com>
2025-02-01 01:14:18 -1000

    changes to button in hero file

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
       <h1 className="lg:text-8xl sm:text-6xl font-semibold p-4 tracking-tighter bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(255,255,255,.5))] drop-shadow-xl text-transparent bg-clip-text">My League Baseball</h1>
       <p className="[text-shadow:_0_4px_8px_#000000] text-lg text-white drop-shadow-md my-5">Using the Latest AI Tech to Elevate your Baseball Experience</p>
       <div className="flex justify-center mt-5">
-      <Button>Try It Now</Button>
+      <Button/>
       </div>
     </div>
   </section>;
